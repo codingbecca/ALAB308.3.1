0cc92cd6457a3882872d8515fb82dae31ad94c3d
codingbecca <131915898+codingbecca@users.noreply.github.com>
2025-01-15 10:53:11 -0800

    Part 1 completed

--- index.js
+++ index.js
@@ -8,6 +8,17 @@
 // If a number is not divisible by either 3 or 5, log the number.
 // Remember to commit your solution once it is working.
 
+for (let i = 1; i <= 100; i++) {
+    if(i % 3 ===0 && i % 5 === 0){
+        console.log('Fizz Buzz');
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
+    } else if (i % 5 === 0){
+        console.log('Buzz');
+    } else {
+        console.log(i)
+    }
+}
 
 // =============== Part 2: Prime Time ===========================
 // Now we will move onto something slightly more complex.
